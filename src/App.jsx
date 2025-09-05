a57407a6611e11a877055c9be22c1d69a8003f11
Deepak Singh <deepaksingh64630@gmail.com>
2025-09-05 15:22:57 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Notices from './pages/Notices';
-import Admin from './pages/admin.jsx';
+import Admin from './pages/Admin.jsx';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import { AuthProvider } from './state/AuthContext';
